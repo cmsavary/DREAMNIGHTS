18610592d4a315e20617f3bcedf4fb4ece04dd39
Céline Savary <cmsavary@yahoo.fr>
2023-10-05 19:29:53 +0200

    validations models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :room
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,5 @@
 class Room < ApplicationRecord
   belongs_to :hotel
+  validates :capacity, presence: true, numericality: { only_integer: true, greater_than: 1 }
+  validates :price_per_night, presence: true, numericality: { greater_than_or_equal_to: 1 }
 end
